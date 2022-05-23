ff7fc14d8540dba18951982b29bfa310e865980a
Norman Smart <normansmart14@gmail.com>
2022-05-23 12:23:53 -0400

    commit tree b296d926384826e2e591ed6f07ca5a55f1983f62

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+ function superbowlWin(arr){
+    const result = arr.find(item => item.result === "W");
+
+    return !! result ? result.year : undefined;
+}
+   
+   superbowlWin(record)
